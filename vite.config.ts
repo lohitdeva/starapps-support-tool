e1bd7cf8d5a2a2e27aa8b4643fe8994946a700fd
Lohit Deva <66935143+lohitdeva@users.noreply.github.com>
2025-09-24 22:33:15 +0400

    chore: updated vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
+import { resolve, dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+const rootDir = dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(rootDir, "index.html"),
+        calculator: resolve(rootDir, "calculator/index.html"),
+        urlgen: resolve(rootDir, "urlgen/index.html"),
+      },
+    },
+  },
 });
